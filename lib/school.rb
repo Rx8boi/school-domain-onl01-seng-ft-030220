14fbed68ce02a7b578a3c2850f062dd979ab039b
Charles Butler <cdbutle@outlook.com>
2020-03-16 01:44:56 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,8 +3,20 @@ class School
     @name = name
     @roster - {}
   end
+  
+  def roster
+    @roster
+  end
+  
   def add_student(name, grade)
-    
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false 
+      @roster[grade] = []
+    end
+    @roster[grade] << name
 end
+
+def grade
 
   end
